1cf45abce1f9996cad0f915c6cbe7d713df1fa30
Jason Bobich <jasonbobich@gmail.com>
2018-06-18 21:39:38 -0600

    Although not technically needed, specify database in basic example.

--- example/basic/index.js
+++ example/basic/index.js
@@ -1,5 +1,7 @@
 const Backroad = require('../..'); // require('backroad')
 
-const app = new Backroad();
+const app = new Backroad({
+  mongoURL: 'mongodb://localhost:27017/basic' // Would default to this, anyway.
+});
 
 app.start();
