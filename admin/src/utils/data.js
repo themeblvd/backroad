9fcfc75310574a6836490cd33d0d02f728012282
Jason Bobich <jasonbobich@gmail.com>
2018-06-23 09:36:49 -0600

    Add helper functions for editing a user.

--- admin/src/utils/data.js
+++ admin/src/utils/data.js
@@ -1,15 +1,67 @@
 /**
  * Get Backroad configuration.
+ *
+ * @param {String} part Optional. Specific part of config.
+ * @return {Object}
  */
 export function getConfig(part) {
-  const backroad = window.backroad;
+  const backroad = JSON.parse(window.backroad);
   if (part) return backroad[part];
   return backroad;
 }
 
 /**
+ * Get a specific content type's configuration
+ * object.
+ *
+ * @param {String} endpoint Content type endpoings like `pages`.
+ * @return {Object} Found content type.
+ */
+export function getContentType(endpoint) {
+  const types = getConfig('contentTypes');
+  return types.find(type => type.endpoint === endpoint);
+}
+
+/**
+ * Check to see if a content type ID is
+ * valid or not.
+ *
+ * @param {String} endpoint Content type endpoings like `pages`.
+ * @return {Boolean} Whether valid.
+ */
+export function isValidContentType(endpoint) {
+  return getContentType(endpoint) ? true : false;
+}
+
+/**
+ * Get the singular title for a content type.
+ *
+ * @param {String} endpoint Content type endpoings like `pages`.
+ * @return {String} Singular title.
+ */
+export function getSingularTitle(endpoint) {
+  const type = getContentType(endpoint);
+  if (type && type.name) return type.name;
+  return '';
+}
+
+/**
+ * Get the singular title for a content type.
+ *
+ * @param {String} endpoint Content type endpoings like `pages`.
+ * @return {String} Plural title.
+ */
+export function getPluralTitle(endpoint) {
+  const type = getContentType(endpoint);
+  if (type && type.pluralName) return type.pluralName;
+  return '';
+}
+
+/**
  * Get initial inputs for editing
  * a user.
+ *
+ * @return {Object}
  */
 export function getUserInputs() {
   return {
@@ -24,6 +76,9 @@ export function getUserInputs() {
 
 /**
  * Sanitize user to save.
+ *
+ * @param {Object} data Data to be sanitized.
+ * @return {Object} Sanitized data.
  */
 export function cleanUserData(data) {
   const { first_name, last_name, email, password, password_confirm } = data;
