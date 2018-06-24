ac8981c7c1f5d4bac0192896ae457913094f4252
Jason Bobich <jasonbobich@gmail.com>
2018-06-23 22:18:18 -0600

    Clean up <PirvateRoute> component.

--- admin/src/containers/PrivateRoute.js
+++ admin/src/containers/PrivateRoute.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-class PrivateRoute extends Component {
-  render() {
-    const { isAuthenticated, path } = this.props;
-    const Component = this.props.component;
-    return isAuthenticated ? <Route path={path} component={Component} /> : <Redirect to="/login" />;
-  }
-}
-export default connect(
-  state => state.auth,
-  {}
-)(PrivateRoute);
+/**
+ * Private Route
+ *
+ * This is a wrapper for <Route /> that
+ * restricts access to logged-in users.
+ *
+ * @return {Component}
+ */
+const PrivateRoute = props => {
+  const { isAuthenticated, path, component } = props;
+  return isAuthenticated ? <Route path={path} component={component} /> : <Redirect to="/login" />;
+};
+
+export default connect(state => state.auth)(PrivateRoute);
